037ba0700e76a7cc44e24b59247240e49dead320
Władysław Bodzek <wladek@wikia-inc.com>
2016-02-22 16:29:54 +0100

    Remove builtin formats declarations

--- src/OpenTracing/Basic/Format.php
+++ src/OpenTracing/Basic/Format.php
@@ -5,8 +5,6 @@ namespace OpenTracing\Basic;
 class Format
 {
 
-    const SPLIT_TEXT = 'text';
-    const SPLIT_BINARY = 'binary';
     const PACKED_HTTP_HEADERS = 'packed_http_headers';
     const RAW_HTTP_HEADERS = 'raw_http_headers';
 
