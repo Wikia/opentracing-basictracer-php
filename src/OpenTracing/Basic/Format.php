1316aafdb8c4aa91de8e97d4af8e0e379264e2c1
Władysław Bodzek <wladek@wikia-inc.com>
2016-02-22 16:21:34 +0100

    Initial commit - moved from opentracing-php

---
+++ src/OpenTracing/Basic/Format.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace OpenTracing\Basic;
+
+class Format
+{
+
+    const SPLIT_TEXT = 'text';
+    const SPLIT_BINARY = 'binary';
+    const PACKED_HTTP_HEADERS = 'packed_http_headers';
+    const RAW_HTTP_HEADERS = 'raw_http_headers';
+
+}
---
+++ src/OpenTracing/Basic/Recorder.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace OpenTracing\Basic;
+
+interface Recorder
+{
+
+}
